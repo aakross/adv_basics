93ae5f422efb1d08b2f1a03a80fb660377609f46
aakroos <royal9619@gmail.com>
2024-04-15 23:22:12 -0600

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:adv_basics/start_screen.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Container(
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                colors: [
+                  Color.fromARGB(255, 126, 84, 224),
+                  Color.fromARGB(255, 51, 21, 90),
+                ],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+              ),
+            ),
+            child: const StartScreen()),
+      ),
+    ),
+  );
+}
